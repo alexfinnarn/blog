c55fbd58dc380e3eab15be8124ae012db21599ff
Alex Finnarn <alex.finnarn@gmail.com>
2019-08-12 18:27:11 -0400

    add post about creating redirects

---
+++ content/own-your-content:-seo/index.md
@@ -0,0 +1,16 @@
+---
+title: '#OwnYourContent: Redirects'
+description: ''
+date: '2019-08-14T20:21:38.504Z'
+categories:
+  - JavaScript
+  - Netlify
+  - SEO
+
+published: false
+# canonical_link: https://medium.com/@alexfinnarn/building-slack-bots-february-2019-edition-of-the-columbus-javascript-usergroup-6210772e07b6
+# redirect_from:
+#  - /building-slack-bots-february-2019-edition-of-the-columbus-javascript-usergroup-6210772e07b6
+---
+
+Finally, I want to have some sort of analytics data I can use to help drive the focus of the content I create. Via Medium, I know that my most popular blog post ended up being on upgrading a Vue.js application to use the project's new `vue-cli` over a community-based starter kit I had been using. Unfortunately, I haven't foucsed on any Vue-related work since the time I published those posts, but at least I now know which articles I can use to drive visitors to more Vue or general JavaScript blog posts. I'd like to at least get some numbers on page views and a tally of how many readers made it all the way to the bottom of the page.
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,32 @@
-import React from "react"
+import React, { useEffect } from 'react'
+import { navigate, Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Section from '../components/section'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/')
+    }, 5000)
+  }, [])
+
+  return (
+    <Layout>
+      <Section centered>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>
+          You just hit a page that doesn't exist and will be redirected back to
+          the home page in five seconds.
+        </p>
+        <p>
+          If you are not redirected, <Link to="/">click here</Link>.
+        </p>
+      </Section>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
