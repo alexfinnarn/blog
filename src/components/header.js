08e9ddb7528b12c04da2c73bc736e025396f1b81
Alex Finnarn <alex.finnarn@gmail.com>
2019-10-07 23:54:16 -0400

    messing around with the header and flexbox

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,24 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+ <div style={{ backgroundColor: 'orange' }}>
+   <div style={{ 
+     display: 'flex', 
+     flexDirection: 'row', 
+     justifyContent: 'space-between',
+     margin: '0 auto',
+     maxWidth: 960,
+     padding: '0.75 rem'
+    }}>
+     <h1 style={{flex: 2, marginBottom: '0.25 rem' }}>Foo</h1>
+     <div style={{ flex: 1, display: 'flex', flexDirection: 'row', alignSelf: 'flex-end', alignContent: 'baseline'}}>
+     <h2 style={{ flex: 1}}>Bar</h2>
+     <h2 style={{ flex: 1}}>Bar</h2>
+     <h2 style={{ flex: 1}}>Bar</h2>
+     </div>
+     
+   </div>
+ </div>
 )
 
 Header.propTypes = {
