e7a7c3c2ad2e4aad2972653ca69ef70707e099e6
Alex Finnarn <alex.finnarn@gmail.com>
2019-11-07 22:24:50 -0500

    add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,21 @@ module.exports = {
     es6: true,
     jest: true,
   }, 
-  rules: {
-    strict: 0
-  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended"
   ],
   plugins: [
-    'react',
-    'jsx-a11y',
-    
+    "react",
+    "react-hooks",
+    "jsx-a11y",
   ],
+  rules: {
+    strict: 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
   settings: {
     react: {
       version: "detect"
